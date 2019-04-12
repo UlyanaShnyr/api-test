e849063ee2b28565ad8ebc597ea81a06758da270
unknown <ulyana250188@gmail.com>
2019-04-13 00:18:00 +0300

    increase and decrease click button

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ const database={
 }
 app.use(express.static('public'))
 
-app.get('/increase', function (req, res) {
+app.get('/increase', function (req, res) {    
   database.count++;
-    res.send(`Count is now ${database.count}`);
+  res.set('Acess-Control-Alloe-Origin','*');
+    res.send(JSON.stringify(database.count));
+   
 });
 
 app.get('/decrease', function (req, res) {
     database.count--;
-      res.send(`Count is now ${database.count}`);
+    res.set('Acess-Control-Alloe-Origin','*');
+    res.send(JSON.stringify(database.count));
   });
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
